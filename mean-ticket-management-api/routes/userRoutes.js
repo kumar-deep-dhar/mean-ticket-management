0d9098febb6664e2130112649a5ae4e42c9961b9
Kumar Deep Dhar <68948695+kumar-deep-dhar@users.noreply.github.com>
2023-12-18 23:33:18 +0530

    Update userRoutes.js

--- mean-ticket-management-api/routes/userRoutes.js
+++ mean-ticket-management-api/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controller/userController");
 
 // Register a new user
-router.post("/signup", userController.register);
+router.post("/signup", userController.signup);
 
 // Login user
 router.post("/login", userController.login);
